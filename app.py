b934ec999c0d2c36032d1d27dad5f9f98c207985
Kafui Alordo <kalordo7@gmail.com>
2024-07-05 09:48:16 +0000

    reverse to static data for demo purpose

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.orm import DeclarativeBase
 
-class Base(DeclarativeBase):
-  pass
+# class Base(DeclarativeBase):
+#   pass
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-db = SQLAlchemy(model_class=Base)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+# db = SQLAlchemy(model_class=Base)
 
-def create_tables():
-    with app.app_context():
-        db.create_all()
+# def create_tables():
+#     with app.app_context():
+#         db.create_all()
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100))
+#     complete = db.Column(db.Boolean)
 
+
+todos = []
 
 @app.route('/')
 def index():
@@ -25,32 +27,46 @@ def index():
     show all
     todos
     '''
-    todo_list = Todo.query.all()
-    return render_template('base.html', todo_list=todo_list)
+    # todo_list = Todo.query.all()
+
+    print("from index", todos)
+    return render_template('base.html', todos=todos)
 
 @app.route("/add", methods=["POST"])
 def add():
     # add a new item
     todo_title = request.form.get("title")
-    new_todo = Todo(title=todo_title, complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
+    print("adding..", todo_title)
+    # new_todo = Todo(title=todo_title, complete=False)
+    # db.session.add(new_todo)
+    # db.session.commit()
+
+    new_todo = {
+        "id": len(todos) + 1,
+        "title": todo_title,
+        "complete": False,
+    }
+
+    todos.append(new_todo)
+
+    print(todos)
+
     return redirect(url_for("index"))
 
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
     # update a todo item
-    update_todo = Todo.query.filter_by(id=todo_id).first()
-    update_todo.complete = not update_todo.complete
-    db.session.commit()
+    # update_todo = Todo.query.filter_by(id=todo_id).first()
+    # update_todo.complete = not update_todo.complete
+    # db.session.commit()
     return redirect(url_for("index"))
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
     # update a todo item
-    delete_todo = Todo.query.filter_by(id=todo_id).first()
-    db.session.delete(delete_todo)
-    db.session.commit()
+    # delete_todo = Todo.query.filter_by(id=todo_id).first()
+    # db.session.delete(delete_todo)
+    # db.session.commit()
     return redirect(url_for("index"))
 
 # @app.route('/about')
@@ -58,8 +74,8 @@ def delete(todo_id):
 #     return "About page"
 
 if __name__ == "__main__":
-    db.init_app(app)
-    create_tables()
+    # db.init_app(app)
+    # create_tables()
 
     # with app.app_context():
     #     dummy_todo = Todo(title="Sample Todo", complete=False)  # Set the due date
